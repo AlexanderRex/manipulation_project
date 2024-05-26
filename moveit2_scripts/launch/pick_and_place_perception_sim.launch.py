73438c9f11fc7b6bee1ad5565cfcc87bd26866ab
AlexanderRex <darkknightrises2018@gmail.com>
2024-05-26 10:35:14 +0000

    perceptoin grip added

---
+++ moveit2_scripts/launch/pick_and_place_perception_sim.launch.py
@@ -0,0 +1,40 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    moveit_config = MoveItConfigsBuilder("name", package_name="my_moveit_config").to_moveit_configs()
+
+    action_server_node = Node(
+            package='simple_grasping',
+            executable='basic_grasping_perception_node',
+            name='basic_grasping_perception',
+            parameters=[{'debug_topics': True}],
+            output='screen',
+            emulate_tty=True,
+    )
+    
+    pick_and_place_node = Node(
+            name="pick_and_place_perception_sim",
+            package="moveit2_scripts",
+            executable="pick_and_place_perception_sim",
+            output="screen",
+            parameters=[
+                moveit_config.robot_description,
+                moveit_config.robot_description_semantic,
+                moveit_config.robot_description_kinematics,
+                {'use_sim_time': True},
+        ],
+    )
+    
+    return LaunchDescription([
+        action_server_node,
+        pick_and_place_node
+    ])
---
+++ moveit2_scripts/src/pick_and_place_perception_sim.cpp
@@ -0,0 +1,318 @@
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/msg/display_robot_state.hpp>
+#include <moveit_msgs/msg/display_trajectory.hpp>
+
+#include <chrono>
+#include <thread>
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("move_group_demo");
+
+#include "grasping_msgs/action/find_graspable_objects.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include <inttypes.h>
+#include <iostream>
+#include <memory>
+#include <string>
+
+class GetPoseClient : public rclcpp::Node {
+public:
+  float x = 0.0;
+  float y = 0.0;
+  float z = 0.0;
+
+  using Find = grasping_msgs::action::FindGraspableObjects;
+  using GoalHandleFind = rclcpp_action::ClientGoalHandle<Find>;
+
+  explicit GetPoseClient(
+      const rclcpp::NodeOptions &node_options = rclcpp::NodeOptions())
+      : Node("get_pose_client", node_options), goal_done_(false) {
+    this->client_ptr_ = rclcpp_action::create_client<Find>(
+        this->get_node_base_interface(), this->get_node_graph_interface(),
+        this->get_node_logging_interface(),
+        this->get_node_waitables_interface(), "find_objects");
+
+    this->timer_ =
+        this->create_wall_timer(std::chrono::milliseconds(500),
+                                std::bind(&GetPoseClient::send_goal, this));
+  }
+
+  bool is_goal_done() const { return this->goal_done_; }
+
+  void send_goal() {
+    using namespace std::placeholders;
+
+    this->timer_->cancel();
+
+    this->goal_done_ = false;
+
+    if (!this->client_ptr_) {
+      RCLCPP_ERROR(this->get_logger(), "Action client not initialized");
+    }
+
+    if (!this->client_ptr_->wait_for_action_server(std::chrono::seconds(10))) {
+      RCLCPP_ERROR(this->get_logger(),
+                   "Action server not available after waiting");
+      this->goal_done_ = true;
+      return;
+    }
+
+    auto goal_msg = Find::Goal();
+    goal_msg.plan_grasps = false;
+
+    RCLCPP_INFO(this->get_logger(), "Sending goal");
+
+    auto send_goal_options = rclcpp_action::Client<Find>::SendGoalOptions();
+    send_goal_options.goal_response_callback =
+        std::bind(&GetPoseClient::goal_response_callback, this, _1);
+    send_goal_options.feedback_callback =
+        std::bind(&GetPoseClient::feedback_callback, this, _1, _2);
+    send_goal_options.result_callback =
+        std::bind(&GetPoseClient::result_callback, this, _1);
+    auto goal_handle_future =
+        this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+  }
+
+private:
+  rclcpp_action::Client<Find>::SharedPtr client_ptr_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  bool goal_done_;
+
+  void goal_response_callback(const GoalHandleFind::SharedPtr &goal_handle) {
+    if (!goal_handle) {
+      RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+    } else {
+      RCLCPP_INFO(this->get_logger(),
+                  "Goal accepted by server, waiting for result");
+    }
+  }
+
+  void feedback_callback(GoalHandleFind::SharedPtr,
+                         const std::shared_ptr<const Find::Feedback> feedback) {
+    RCLCPP_INFO(this->get_logger(), "Ignoring feedback...");
+  }
+
+  void result_callback(const GoalHandleFind::WrappedResult &result) {
+    this->goal_done_ = true;
+    switch (result.code) {
+    case rclcpp_action::ResultCode::SUCCEEDED:
+      break;
+    case rclcpp_action::ResultCode::ABORTED:
+      RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+      return;
+    case rclcpp_action::ResultCode::CANCELED:
+      RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+      return;
+    default:
+      RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+      return;
+    }
+
+    RCLCPP_INFO(this->get_logger(), "Result received");
+    for (auto object : result.result->objects) {
+      RCLCPP_INFO(this->get_logger(), "Type: %d",
+                  object.object.primitives[0].type);
+      RCLCPP_INFO(this->get_logger(), "x: %f",
+                  object.object.primitives[0].dimensions[0]);
+      RCLCPP_INFO(this->get_logger(), "Y: %f",
+                  object.object.primitives[0].dimensions[1]);
+      RCLCPP_INFO(this->get_logger(), "Z: %f",
+                  object.object.primitives[0].dimensions[2]);
+      x = object.object.primitives[0].dimensions[0];
+      y = object.object.primitives[0].dimensions[1];
+      z = object.object.primitives[0].dimensions[2];
+    }
+
+    //}
+  }
+}; // class GetPoseClient
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+
+  auto action_client = std::make_shared<GetPoseClient>();
+
+  while (!action_client->is_goal_done()) {
+    rclcpp::spin_some(action_client);
+  }
+
+  rclcpp::NodeOptions node_options;
+
+  node_options.automatically_declare_parameters_from_overrides(true);
+  auto move_group_node =
+      rclcpp::Node::make_shared("move_group_interface_tutorial", node_options);
+
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(move_group_node);
+  std::thread([&executor]() { executor.spin(); }).detach();
+
+  static const std::string PLANNING_GROUP_ARM = "ur_manipulator";
+  static const std::string PLANNING_GROUP_GRIPPER = "gripper";
+
+  moveit::planning_interface::MoveGroupInterface move_group_arm(
+      move_group_node, PLANNING_GROUP_ARM);
+  moveit::planning_interface::MoveGroupInterface move_group_gripper(
+      move_group_node, PLANNING_GROUP_GRIPPER);
+
+  const moveit::core::JointModelGroup *joint_model_group_arm =
+      move_group_arm.getCurrentState()->getJointModelGroup(PLANNING_GROUP_ARM);
+  const moveit::core::JointModelGroup *joint_model_group_gripper =
+      move_group_gripper.getCurrentState()->getJointModelGroup(
+          PLANNING_GROUP_GRIPPER);
+
+  // Get Current State
+  moveit::core::RobotStatePtr current_state_arm =
+      move_group_arm.getCurrentState(10);
+  moveit::core::RobotStatePtr current_state_gripper =
+      move_group_gripper.getCurrentState(10);
+
+  std::vector<double> joint_group_positions_arm;
+  std::vector<double> joint_group_positions_gripper;
+  current_state_arm->copyJointGroupPositions(joint_model_group_arm,
+                                             joint_group_positions_arm);
+  current_state_gripper->copyJointGroupPositions(joint_model_group_gripper,
+                                                 joint_group_positions_gripper);
+
+  move_group_arm.setStartStateToCurrentState();
+  move_group_gripper.setStartStateToCurrentState();
+
+  // Go Home
+  RCLCPP_INFO(LOGGER, "Going Home");
+
+  // joint_group_positions_arm[0] = 0.00;  // Shoulder Pan
+  joint_group_positions_arm[1] = -2.50; // Shoulder Lift
+  joint_group_positions_arm[2] = 1.50;  // Elbow
+  joint_group_positions_arm[3] = -1.50; // Wrist 1
+  joint_group_positions_arm[4] = -1.55; // Wrist 2
+  // joint_group_positions_arm[5] = 0.00;  // Wrist 3
+
+  move_group_arm.setJointValueTarget(joint_group_positions_arm);
+
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan_arm;
+  bool success_arm = (move_group_arm.plan(my_plan_arm) ==
+                      moveit::core::MoveItErrorCode::SUCCESS);
+
+  move_group_arm.execute(my_plan_arm);
+
+  // Pregrasp
+  RCLCPP_INFO(LOGGER, "Pregrasp Position");
+
+  geometry_msgs::msg::Pose target_pose1;
+  target_pose1.orientation.x = -1.0;
+  target_pose1.orientation.y = 0.00;
+  target_pose1.orientation.z = 0.00;
+  target_pose1.orientation.w = 0.00;
+  target_pose1.position.x = action_client->x + 0.322096; // calibration number
+  target_pose1.position.y = action_client->y - 0.021036;
+  target_pose1.position.z = action_client->z + 0.206675;
+
+  RCLCPP_INFO(move_group_node->get_logger(), "New target positions set: x = %f, y = %f, z = %f",
+                target_pose1.position.x,
+                target_pose1.position.y,
+                target_pose1.position.z);
+
+  move_group_arm.setPoseTarget(target_pose1);
+
+  success_arm = (move_group_arm.plan(my_plan_arm) ==
+                 moveit::core::MoveItErrorCode::SUCCESS);
+
+  move_group_arm.execute(my_plan_arm);
+
+  // Open Gripper
+
+  RCLCPP_INFO(LOGGER, "Open Gripper!");
+
+  move_group_gripper.setNamedTarget("gripper_open");
+
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan_gripper;
+
+  bool success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
+                          moveit::core::MoveItErrorCode::SUCCESS);
+
+  move_group_gripper.execute(my_plan_gripper);
+
+  // Approach
+  RCLCPP_INFO(LOGGER, "Approach to object!");
+
+  std::vector<geometry_msgs::msg::Pose> approach_waypoints;
+  target_pose1.position.z -= 0.05;
+  approach_waypoints.push_back(target_pose1);
+
+  target_pose1.position.z -= 0.05;
+  approach_waypoints.push_back(target_pose1);
+
+  moveit_msgs::msg::RobotTrajectory trajectory_approach;
+  const double jump_threshold = 0.0;
+  const double eef_step = 0.01;
+
+  double fraction = move_group_arm.computeCartesianPath(
+      approach_waypoints, eef_step, jump_threshold, trajectory_approach);
+
+  move_group_arm.execute(trajectory_approach);
+
+  // Close Gripper
+
+  RCLCPP_INFO(LOGGER, "Close Gripper!");
+
+  joint_group_positions_gripper[2] = 0.645;
+
+  move_group_gripper.setJointValueTarget(joint_group_positions_gripper);
+
+  success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
+                     moveit::core::MoveItErrorCode::SUCCESS);
+
+  move_group_gripper.execute(my_plan_gripper);
+
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+
+  // Retreat
+
+  RCLCPP_INFO(LOGGER, "Retreat from object!");
+
+  std::vector<geometry_msgs::msg::Pose> retreat_waypoints;
+  target_pose1.position.z += 0.05;
+  retreat_waypoints.push_back(target_pose1);
+
+  target_pose1.position.z += 0.05;
+  retreat_waypoints.push_back(target_pose1);
+
+  moveit_msgs::msg::RobotTrajectory trajectory_retreat;
+
+  fraction = move_group_arm.computeCartesianPath(
+      retreat_waypoints, eef_step, jump_threshold, trajectory_retreat);
+
+  move_group_arm.execute(trajectory_retreat);
+
+  // Place
+
+  RCLCPP_INFO(LOGGER, "Rotating Arm");
+
+  current_state_arm = move_group_arm.getCurrentState(10);
+  current_state_arm->copyJointGroupPositions(joint_model_group_arm,
+                                             joint_group_positions_arm);
+
+  joint_group_positions_arm[0] = 1.0; // Shoulder Pan
+
+  move_group_arm.setJointValueTarget(joint_group_positions_arm);
+
+  success_arm = (move_group_arm.plan(my_plan_arm) ==
+                 moveit::core::MoveItErrorCode::SUCCESS);
+
+  move_group_arm.execute(my_plan_arm);
+
+  // Open Gripper
+
+  RCLCPP_INFO(LOGGER, "Release Object!");
+
+  move_group_gripper.setNamedTarget("gripper_open");
+
+  success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
+                     moveit::core::MoveItErrorCode::SUCCESS);
+
+  move_group_gripper.execute(my_plan_gripper);
+
+  rclcpp::shutdown();
+  return 0;
+}
